53103cb75900e013c58f612ba16fcfea0b3f2b26
mebiplob <syedafnanfreelancer@gmail.com>
2024-02-03 19:40:19 +0600

    React Counter Project

--- react_counter/src/App.jsx
+++ react_counter/src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [number, setNumber] = useState(10);
+
+  let addNumber = () => {
+    if(number <= 19 && number >= 0){
+      console.log(number)
+      setNumber(number + 1)
+    }
+    
+  }
+
+  let removeNumber = () => {
+    if(number <=20 && number >0){
+      console.log(number)
+      setNumber(number - 1)
+    }
+    
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='d-flex justify-center items-center relative'>
+      <h1 className=' text-4xl text-center bg-green-600 text-yellow-100 rounded px-4 py-1 mb-4'>React Counter Project</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+      <h3 className='text-2xl mb-5'>Number {number}</h3>
+
+      <button onClick={addNumber} className='btn bg-teal-300 text-black mr-5 px-3 py-1 rounded'>Add Number {number}</button>
+      <button onClick={removeNumber} className='btn bg-teal-300 text-black px-3 py-1 rounded'>Remove Number {number}</button>
+
     </>
   )
 }
